6f2fa862adbbaadaa031646736604be6457ac14e
sydykov77 <erjansydykov77>
2020-04-01 21:47:59 +0630

    Initial commit

---
+++ app/src/main/java/com/erzhan/android1_ls1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.erzhan.android1_ls1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
